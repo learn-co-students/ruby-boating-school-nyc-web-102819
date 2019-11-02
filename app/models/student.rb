e6835d0ccae2c240c0304f73c22b3130be74ece4
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-02 12:22:59 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+    attr_accessor :first_name, :last_name
+    
+    @@all= []
+    
+    def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+    end
+    
+    def self.all
+      @@all
+    end
+
+    def add_boating_test(name, status, instructor)
+    #     #should initialize a new boating test with a Student (Object),
+    #     #a boating test name (String), a boating test status (String),
+    #     #and an Instructor (Object)
+    
+        BoatingTest.new(self, name, status, instructor)
+    end
+
+    def self.find_student(name)
+        self.all.find do |student|
+            student.first_name == name
+        end
+    end
+
+    def grade_percentage
+        # find the tests for specific student we call this method on though all of the BoatingTests
+        student_test = BoatingTest.all.find_all { |test| test.student.first_name ==  self.first_name }
+        total_tests = student_test.length.to_f
+        total_passed = student_test.find_all {|test|test.status ==  "passed"}
+        num_passed  = total_passed.length.to_f
+        percentage = (num_passed/total_tests)*100
+    end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -5,6 +5,17 @@ def reload
 end
 
 # Insert code here to run before hitting the binding.pry
+barak = Student.new("Barak")
+carlos = Student.new("Carlos")
+tim = Student.new("Tim")
+
+steve = Instructor.new("Steven")
+dana = Instructor.new("Dana")
+
+# student, name, status, instructor
+test1 = BoatingTest.new(barak,"Mod1 -test", "not-graded", steve)
+test2 = BoatingTest.new(carlos,"abc", "not-graded", dana)
+
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
