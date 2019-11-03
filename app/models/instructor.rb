ab9de084e87cf3ba781d901f2c9783308a759cab
joffitzer <joffitzer@gmail.com>
2019-11-03 14:56:05 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
 
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end 
+
+    def pass_student(student, boating_test_name, boating_test)
+        if BoatingTest.all.find { |bt| bt.student == student && bt.boating_test_name == boating_test_name } #there is a boating test whose name and student match the values passed in
+            boating_test.boating_test_status = "passed"
+            boating_test
+        else
+            BoatingTest.new(student, boating_test_name, "passed", self)
+        end 
+
+        # if there is a boating test whose name and student match the values passed in, update the status to "passed"
+        # if there is no matching test, this should create a test with the student, boat test name, and the status "passed"
+        # either way, it should return the boating test instance 
+        
+    end 
+
+    def fail_student(student, boating_test_name, boating_test)
+        if BoatingTest.all.find { |bt| bt.student == student && bt.boating_test_name == boating_test_name }
+            boating_test.boating_test_status = "failed"
+            boating_test
+        else 
+            BoatingTest.new(student, boating_test_name, "failed", self) 
+        end 
+    end 
+
+    def self.all
+        @@all
+    end 
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,43 @@
 class Student
 
+    attr_accessor :first_name 
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+
+        @@all << self 
+    end 
+
+    def add_boating_test(boating_test_name, boating_test_status, instructor)
+        BoatingTest.new(self, boating_test_name, boating_test_status, instructor)
+    end 
+
+    def all_tests
+        BoatingTest.all.select { |bt| bt.student == self }
+    end 
+
+    def passed_tests
+        all_tests.select { |bt| bt.boating_test_status == "passed"}
+    end 
+
+    def grade_percentage
+        #should return the percentage of tests that the student has passed 
+        #so if a student's boating_test_status= "passed" 3/9 times, it should return a float of 33.33 
+
+        #get an array of all of the student's tests. how long is it?
+        #make an array of all of the passing ones. how long is it?  
+        #divide length of passed tests array by length of total tests array
+        self.passed_tests.length.to_f / self.all_tests.length.to_f * 100
+    end 
+
+    def self.find_student(name)
+        self.all.find { |student| student.first_name = name }
+    end 
+
+    def self.all
+        @@all
+    end 
+
 end
