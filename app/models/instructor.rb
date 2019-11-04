d132040cae6fa2bfdab9cff2f93cc22f0694878f
Aaron Klotnia <aklotnia@gmail.com>
2019-11-04 17:43:06 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Instructor
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, test_name)
+        if BoatingTest.all.find {|boatingtest_obj| boatingtest_obj.instructor == self && boatingtest_obj.student == student && boatingtest_obj.boating_test_name == test_name}
+            target = BoatingTest.all.select {|boatingtest_obj| boatingtest_obj.instructor == self && boatingtest_obj.student == student && boatingtest_obj.boating_test_name == test_name}.uniq
+            # binding.pry
+            target[0].boating_test_status = "passed"
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        if BoatingTest.all.find {|boatingtest_obj| boatingtest_obj.instructor == self && boatingtest_obj.student == student && boatingtest_obj.boating_test_name == test_name}
+            target = BoatingTest.all.select {|boatingtest_obj| boatingtest_obj.instructor == self && boatingtest_obj.student == student && boatingtest_obj.boating_test_name == test_name}.uniq
+            # binding.pry
+            target[0].boating_test_status = "failed"
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,31 @@
 class Student
 
 end
+class Student
+    attr_reader :first_name
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        student_objs = BoatingTest.all.map {|boatingtest_obj| boatingtest_obj.student}
+        student_objs.select {|student_obj| student_obj.first_name == first_name}.uniq
+    end
+
+    def grade_percentage
+        my_boating_tests = BoatingTest.all.select {|boatingtest_obj| boatingtest_obj.student == self}
+        passed_boating_tests = my_boating_tests.select {|boatingtest_obj| boatingtest_obj.boating_test_status == "passed"}
+        (passed_boating_tests.length.to_f / my_boating_tests.length).round(2)
+    end
+end
