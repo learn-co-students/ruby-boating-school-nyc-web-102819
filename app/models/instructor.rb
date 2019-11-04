926dd987d080d27637df506ef2b801d7b56326c2
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-04 00:42:22 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def boating_test(student, test_name)
+    b_test = BoatingTest.all.find do |bt|
+      bt.student == student && bt.test_name == test_name
+    end
+    b_test ? (b_test) : (BoatingTest.new(student, test_name, "pending", self))
+  end
+
+  def pass_student(student, test_name)
+    b_test = boating_test(student, test_name)
+    b_test.test_status = "passed"
+    b_test
+  end
+
+  def fail_student(student, test_name)
+    b_test = boating_test(student, test_name)
+    b_test.test_status = "failed"
+    b_test
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,42 @@
 class Student
 
+  attr_reader :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.find do |student|
+      student.first_name == first_name
+    end
+  end
+
+  def boating_tests
+    boating_tests = BoatingTest.all.select do |b_test|
+      b_test.student == self
+    end
+  end
+
+  def grade_percentage
+    passed = 0.0
+    failed = 0.0
+    boating_tests.each do |b_test|
+      passed += 1 if b_test.test_status == "passed";
+      failed += 1 if b_test.test_status == "failed"
+    end
+    passed / (passed + failed) * 100
+  end
+
 end
