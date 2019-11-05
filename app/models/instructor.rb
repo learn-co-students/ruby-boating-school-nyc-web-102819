bb77b8fec5020e7b0dbf50fc0743c696b62547cb
crpineda1 <carlos.r.pineda@gmail.com>
2019-11-04 23:44:44 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,71 @@
 class Instructor
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def test_given?(student, test_name)
+        BoatingTest.all.any? do |each| 
+            each.instructor == self &&
+            each.student == student &&
+            each.test_name == test_name
+        end
+
+    end
+
+
+
+    def pass_student(student,test_name)
+        
+        test_given = BoatingTest.all.any? do |each| 
+            each.instructor == self &&
+            each.student == student &&
+            each.test_name == test_name
+        end
+
+        if test_given
+            BoatingTest.all.select do |each|
+                each.test_status = "passed" if each.instructor == self &&
+                each.student == student &&
+                each.test_name == test_name
+                
+            end
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+
+    end
+    
+
+    def fail_student(student,test_name)
+        
+        test_given = BoatingTest.all.any? do |each| 
+            each.instructor == self &&
+            each.student == student &&
+            each.test_name == test_name
+        end
+
+        if test_given
+            BoatingTest.all.select do |each|
+                each.test_status = "failed" if each.instructor == self &&
+                each.student == student &&
+                each.test_name == test_name
+                
+            end
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+
+    end
+    
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,10 +4,34 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+# students 
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+# instructors
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+ 
+# test
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
+# find student
+find1 = Student.find_student("Spongebob")
+find2 = Student.find_student("Patrick")
+
+# pass student
+pass1 = puff.pass_student(spongebob,"Don't Crash 101")
+pass2 = puff.pass_student(patrick,"Don't Crash 101")
+
+# fail student
+fail1 = krabs.fail_student(spongebob,"Power Steering 201")
+fail2 = krabs.fail_student(patrick,"Power Steering 201")
+
+
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
 
+puts "EXIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIT EXIT EXIT EXIT"
