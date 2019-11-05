2aa6c07188e61d44d138d7a4eae84757d980d7e1
eliskim27 <eliskim27@gmail.com>
2019-11-04 22:32:33 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
+    attr_accessor :instructor, :student, :test_name, :test_status
+    @@all = []
 
+    def initialize(instructor)
+        @instructor = instructor
+        @@all << self
+    end
+###############
+    def self.all
+        @@all
+    end
+
+    def all_tests                       #helper method
+        BoatingTest.all                 #array of all tests
+    end
+    
+    def pass_student(student, test_name)
+        all_tests.find do |test|
+            if test.student && test.test_name
+            else
+                BoatingTest.new(student, test_name, test_status, self)
+            end
+            test.test_status = "passed"
+        end   
+    end
+
+    def fail_student(student, test_name)
+        all_tests.find do |test|
+            if test.student && test.test_name
+            else
+                BoatingTest.new(student, test_name, test_status, self)
+            end
+            test.test_status = "failed"
+        end   
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,39 @@
 class Student
+    attr_reader :student
+    @@all = []
+
+    def initialize (student)
+        @student = student
+        @@all << self
+    end
+#############
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(name)
+        all.find do |student|
+            student.first_name == name
+        end
+    end
+
+    def all_student_tests
+        BoatingTest.all.select do |test|
+            test.student == self
+        end
+    end
+
+    def grade_percentage
+        pass = all_student_tests.count do |test|
+            test.test_status == "passed"
+        end
+        (pass.to_f/all_student_tests.count.to_f)*100
+        # binding.pry
+
+    end
 
 end
